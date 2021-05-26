d9bfe5fca0a28bfe5d070b77f68e4e8b137cd072
Sami Alaoui <4ndroidgeek@gmail.com>
2021-05-26 14:59:44 +0100

    cdnify more aggressively

--- _plugins/cdnify-images.rb
+++ _plugins/cdnify-images.rb
@@ -1,10 +1,9 @@
-Jekyll::Hooks.register :posts, :pre_render do |post, payload|
+Jekyll::Hooks.register [:pages, :posts], :pre_render do |post, payload|
   if Jekyll.env == "production"
     docExt = post.extname.tr('.', '')
-    # only process if we deal with a markdown file
     if payload['site']['markdown_ext'].include? docExt
-      newContent = post.content.gsub(/\!\[(.*)\]\(\/assets\/media\/(.+)\)/, '![\1](https://i.0x41.cf/b/\2)')
-      post.content = newContent
+      post.content = post.content.gsub(/\[(.*?)\]\((?:{{\s*?site.baseurl\s*?}})?\/assets\/media\/(.*?)\)/, '[\1](https://i.0x41.cf/b/\2)')
     end
+    post.content = post.content.gsub(/(\s(?:src|href))="(?:{{\s*?site.baseurl\s*?}})?\/assets\/media\/(.*?)"/, '\1="https://i.0x41.cf/b/\2"')
   end
 end
